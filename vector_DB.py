3dd30451edef779d96f94a9271a96ec9caa63db2
Vinayak Mishra <vk2005917@gmail.com>
2026-08-25 16:12:34 +0530

    better files

--- vector_DB.py
+++ vector_DB.py
@@ -3,6 +3,7 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import Distance, VectorParams, PointStruct, Filter, FieldCondition, MatchValue
 from chuncking import split_text_into_chunks
 from uuid import uuid4
+import os
 from utils import debug_print
 from langchain.tools import tool
 from websocket import send_tool_update
@@ -31,6 +32,33 @@ if not client.collection_exists(COLLECTION_NAME):
 
 @tool("upsert_file", description="Upsert a file into the vector database.")
 def upsert_file(file_path, session_id):
+    chunks = split_text_into_chunks(file_path)
+    texts = [chunk["text"] for chunk in chunks]
+
+    # Generate embeddings
+    vectors = embedding_model.encode(texts, normalize_embeddings=True)
+
+    points = []
+
+    for chunk, vector in zip(chunks, vectors):
+        points.append(
+            PointStruct(
+                id=str(uuid4()),
+                vector=vector.tolist(),
+                payload={
+                    "text": chunk["text"],
+                    "source": chunk["source"],
+                    "chunk_index": chunk["chunk_index"],
+                    "session_id": session_id,
+                },
+            )
+        )
+
+
+    client.upsert(collection_name=COLLECTION_NAME, points=points)
+    return len(points)
+
+def upsert_file_func(file_path, session_id):
     chunks = split_text_into_chunks(file_path)
     texts = [chunk["text"] for chunk in chunks]
 
@@ -91,24 +119,17 @@ def retrieve_top_k(query, session_id, k=5):
     return results.points
 
 if __name__ == "__main__":
-    file_path = "Files\Vinayak Mishra Resume.pdf"
+    #chunk and upsert all files into folder Files
     session_id = "public"
 
+    files_dir = "./Files"
+    supported_exts = (".pdf", ".txt", ".md", ".docx", ".csv", ".xlsx", ".json")
 
-    num_points = upsert_file(file_path, session_id)
-    print(f"Inserted {num_points} points into the collection '{COLLECTION_NAME}'.")
-
-
-    keyword = input("Enter a keyword to search: ")
-    results = retrieve_top_k(keyword, session_id, k=5)
-
-    for i, point in enumerate(results):
-        print(f"Result {i + 1}:")
-        print(f"Score: {point.score}")
-        print(f"Text: {point.payload['text'][:15]}")
-        print(f"Source: {point.payload['source']}")
-        print(f"Chunk Index: {point.payload['chunk_index']}")
-        print("="*25)
+    for filename in os.listdir(files_dir):
+        file_path = os.path.join(files_dir, filename)
+        if os.path.isfile(file_path) and filename.lower().endswith(supported_exts):
+            count = upsert_file_func(file_path, session_id)
+            debug_print(f"Upserted {count} chunks from {filename}")
 
     client.close()
     
